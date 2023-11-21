c45ce0e27081977c258ff6a52da25fcaffb10817
juanlavieri <juan.lavieri@gmail.com>
2023-11-21 13:12:33 -0600

    Enhance updateHorse to handle file uploads

--- server/controllers/horseController.js
+++ server/controllers/horseController.js
@@ -21,29 +21,36 @@ exports.createHorse = async (req, res) => {
 
 // Update a horse
 exports.updateHorse = async (req, res) => {
-    try {
-      const horse = await Horse.findByIdAndUpdate(req.params.horseId, req.body, { new: true });
-      if (!horse) {
-        return res.status(404).json({ message: 'Horse not found' });
-      }
-      res.json(horse);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Server error');
+  try {
+    const horseUpdateData = { ...req.body };
+
+    // If there's a file uploaded, add its path to the update data
+    if (req.file) {
+      horseUpdateData.medicalDocuments = req.file.path; // Or handle it as needed
     }
-  };
+
+    const horse = await Horse.findByIdAndUpdate(req.params.horseId, horseUpdateData, { new: true });
+    if (!horse) {
+      return res.status(404).json({ message: 'Horse not found' });
+    }
+
+    res.json(horse);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+};
   
-  // Delete a horse
-  exports.deleteHorse = async (req, res) => {
-    try {
-      const horse = await Horse.findByIdAndDelete(req.params.horseId);
-      if (!horse) {
-        return res.status(404).json({ message: 'Horse not found' });
-      }
+// Delete a horse
+exports.deleteHorse = async (req, res) => {
+  try {
+    const horse = await Horse.findByIdAndDelete(req.params.horseId);
+    if (!horse) {
+      return res.status(404).json({ message: 'Horse not found' });      }
       res.json({ message: 'Horse deleted' });
     } catch (error) {
       console.error(error);
       res.status(500).send('Server error');
-    }
-  };
+  }
+};
 // Implement other CRUD operations similarly
--- server/routes/horseRoutes.js
+++ server/routes/horseRoutes.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const horseController = require('../controllers/horseController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // Adjust based on your storage solution
 
 router.post('/', authMiddleware, horseController.createHorse);
 router.put('/:horseId', authMiddleware, horseController.updateHorse);
 router.delete('/:horseId', authMiddleware, horseController.deleteHorse);
+// Add a route for updating horse with file upload
+router.put('/:horseId', authMiddleware, upload.single('medicalDocument'), horseController.updateHorse);
+
 // Define other routes (GET, PUT, DELETE) similarly
 
 module.exports = router;
