07d4413898dde1109dd1d5db390b93d63d449c4e
juanlavieri <juan.lavieri@gmail.com>
2023-11-20 21:13:49 -0600

    Implement user profile update functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -58,3 +58,22 @@ exports.getProfile = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Update user profile
+exports.updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id; // Get user ID from request, adjust based on your auth setup
+    const { name, email } = req.body; // Extract fields from request body
+
+    // Update user in database, this is just an example, adjust according to your database schema
+    const updatedUser = await User.findByIdAndUpdate(userId, { name, email }, { new: true });
+    
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/userController');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/profile', userController.getProfile);
+router.put('/profile', authMiddleware, userController.updateProfile); // Use authMiddleware if you have authentication
 
 
 module.exports = router;
