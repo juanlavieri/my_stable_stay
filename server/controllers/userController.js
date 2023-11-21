dc2d8ef794a746129dbf7b12fe37abd6826cd33d
juanlavieri <juan.lavieri@gmail.com>
2023-11-20 20:19:10 -0600

    Added Route and controller for getProfile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,3 +43,18 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Get user profile
+exports.getProfile = async (req, res) => {
+  try {
+    // Assuming you have access to the user's ID from the request
+    // For example, using req.userId if you're using JWT tokens
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get('/profile', userController.getProfile);
+
 
 module.exports = router;
