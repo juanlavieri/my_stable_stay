da8ad48bb1db7385c753a428184f1e9bccd65b16
juanlavieri <juan.lavieri@gmail.com>
2023-11-20 21:45:06 -0600

    Implement user authentication and profile update

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
